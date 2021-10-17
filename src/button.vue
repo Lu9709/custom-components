160b9c3f64a46f3b1b1cace9462c644637ff277d
白褶 <919041098@qq.com>
2021-10-17 17:22:34 +0800

    将svg 代码整理到icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-    <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`"></use></svg>
-    <div class="content"><slot/></div>
+    <g-icon v-if="icon" :name="icon" class="icon"/>
+    <div class="content"><slot></slot></div>
   </button>
 </template>
 <script>
